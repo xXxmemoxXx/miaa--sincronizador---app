6cd6afc32bba00351e7a52f61b1a863c41567c18
Miaa <pedroguillermomiaa@gmail.com>
2026-02-04 14:04:53 -0600

    Update sincronizador_app.py

--- sincronizador_app.py
+++ sincronizador_app.py
@@ -2222,7 +2222,7 @@ with tab1:
     st.markdown(f'<div class="consola-log">{log_txt}</div>', unsafe_allow_html=True)
 
 with tab2:
-    st.title("🖥️ Sincronizador")
+    st.title("🖥️ MIAA Sincronizador app")
     if st.button("🔄 Refrescar Tabla", use_container_width=True):
         st.cache_data.clear()
         st.rerun()
@@ -2231,6 +2231,7 @@ with tab2:
     eng_pg = create_engine(f"postgresql://{DB_POSTGRES['user']}:{p_pg}@{DB_POSTGRES['host']}:{DB_POSTGRES['port']}/{DB_POSTGRES['db']}")
     df_pg = pd.read_sql('SELECT * FROM public."Pozos" LIMIT 500', eng_pg)
     st.dataframe(df_pg, use_container_width=True)
+
 
 
 
