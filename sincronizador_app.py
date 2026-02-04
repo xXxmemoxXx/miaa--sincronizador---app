d6a6086539904c5064aa862095df6630ca9378d9
Miaa <pedroguillermomiaa@gmail.com>
2026-02-04 14:54:53 -0600

    Update sincronizador_app.py

--- sincronizador_app.py
+++ sincronizador_app.py
@@ -2222,7 +2222,7 @@ def ejecutar_sincronizacion_total():
 tab1, tab2 = st.tabs(["🔄 Sincronizador", "📊 Datos (QGIS)"])
 
 with tab1:
-    st.title("🖥️ MIAA Sincronizador app")
+    st.title("🖥️ Sincronizador app")
     with st.container(border=True):
         if st.button("🚀 CARGA DE DATOS", use_container_width=True, type="primary"):
             st.session_state.last_logs = ejecutar_sincronizacion_total()
@@ -2241,6 +2241,7 @@ with tab2:
     eng_pg = create_engine(f"postgresql://{DB_POSTGRES['user']}:{p_pg}@{DB_POSTGRES['host']}:{DB_POSTGRES['port']}/{DB_POSTGRES['db']}")
     df_pg = pd.read_sql('SELECT * FROM public."Pozos" LIMIT 500', eng_pg)
     st.dataframe(df_pg, use_container_width=True)
+
 
 
 
